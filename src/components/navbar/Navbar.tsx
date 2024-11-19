ead3ab51768024436e73b026e77b85c8d6b266ec
Z1Kaladin <2214039@utrivieramaya.edu.mx>
2024-11-18 19:41:41 -0500

    cambio al navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 import "./navbar.scss"
-
+import { Users } from "../../data"
 const Navbar = () => {
   return (
+    
     <div className="navbar">
       <div className="logo">
         <img src="" alt="" />
@@ -9,12 +10,15 @@ const Navbar = () => {
       </div>
       <div className="icons">
         <img src="/settings.svg" alt="" className="icon" />
+        {Users.map( User =>
         <div className="user">
-          <img src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-          <span>User</span>
+          
+          <span>{User.name}</span>
         </div>
+        )}
       </div>
     </div>
+  
   )
 }
 
